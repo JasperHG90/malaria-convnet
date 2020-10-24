e37060bdfc71a2a5b9779447f138a6afd5a16556
Jasper Ginn <jasperginn@gmail.com>
2020-10-24 11:58:08 +0200

    Finalized test

--- tests/test_convnet.py
+++ tests/test_convnet.py
@@ -1,6 +1,7 @@
 import math
 from src.convNet.model import compute_padding_size, convNet, convolutional_block
 import torch
+import pytest
 
 
 def compute_layer_size(image_dim: int, kernel_size: int, stride: int, padding: int=0) -> int:
@@ -9,7 +10,6 @@ def compute_layer_size(image_dim: int, kernel_size: int, stride: int, padding: i
 
 def test_compute_padding_size():
     """Test computing padding size"""
-
     D1 = 32
     P = compute_padding_size(D1, 5, 1)
     # Compute output size
@@ -34,14 +34,20 @@ def test_convolutional_block():
     x = CB(x)
     # Shape should be 2, 6, 14, 14
     assert tuple(x.shape) == (2, 6, 14, 14)
-    # Test padding = "same"
-    CB = convolutional_block(3, 6, 5, "same")
+    # Test padding = "same" but not specified what input dim is
+    with pytest.raises(Exception):
+        CB = convolutional_block(3, 6, 5, "same")
+    CB = convolutional_block(3, 6, 5, "same", image_dim=32)
     x = torch.rand(2, 3, 32, 32)
     x = CB(x)
     assert tuple(x.shape) == (2, 6, 16, 16)
 
 
-
 def test_convNet():
     """Test convNet implementation"""
     CN = convNet(dropout=0.3)
+    x = torch.rand(2, 3, 32, 32)
+    x = CN(x)
+    assert tuple(x.shape) == (2, 1)
+
+
